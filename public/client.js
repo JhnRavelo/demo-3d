2dab5b3554efc19aba3eede9ab2d893efbcbd5c2
JhnRavelo <johnravelo135@gmail.com>
2023-08-17 16:25:48 +0300

    DirectionnalLight and HemisphereLight

--- public/client.js
+++ public/client.js
@@ -2,10 +2,10 @@ import * as THREE from 'three';
 
 import { MTLLoader } from './jsm/loaders/MTLLoader.js';
 import { OBJLoader } from './jsm/loaders/OBJLoader.js';
-import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
 // import { RGBLoader } from './jsm/loaders/RGBLoader.js'
 
-let camera, controls, scene, renderer,loader,model,hemiLight,spotLight,pointLight;
+let camera, controls, scene, renderer,loader,model,hemiLight,spotLight,pointLight,dirLight;
 
 init();
 animate();
@@ -86,36 +86,62 @@ renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+// renderer.setClearColor(0xfffff5)
 renderer.setClearColor(0xE86222)
 renderer.toneMapping = THREE.ReinhardToneMapping
 renderer.toneMappingExposure = 3.5
 // renderer.shadowMap.enabled = true
 //
 
-controls = new  OrbitControls( camera, renderer.domElement );
+controls = new OrbitControls( camera, renderer.domElement );
 controls.update()
 scene.add(controls)
 
 // -------control of the light---------
 
-hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 6)
-hemiLight.castShadow = true
+hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 2.5)
+hemiLight.color.setHSL(0.6,1,0.6)
+hemiLight.groundColor.setHSL(0.095,1,0.75)
+hemiLight.position.set(0,20,0)
+
+// hemiLight.castShadow = true
 scene.add(hemiLight)
-spotLight = new THREE.SpotLight(0xffa95c,4)
-spotLight.castShadow = true
+scene.add(new THREE.HemisphereLightHelper(hemiLight))
+
+dirLight = new THREE.DirectionalLight(0xffffff,2)
+dirLight.color.setHSL(0.1,1,0.95)
+dirLight.position.set(-1,1.75,1)
+dirLight.position.multiplyScalar(30)
+scene.add(dirLight)
+scene.add(new THREE.DirectionalLightHelper(dirLight))
+scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
+dirLight.castShadow = true
+dirLight.shadow.mapSize.width = 2048
+dirLight.shadow.mapSize.height = 2048
+
+var distance = 20
+
+dirLight.shadow.camera.left = -distance
+dirLight.shadow.camera.right = distance
+dirLight.shadow.camera.top = distance
+dirLight.shadow.camera.bottom = -distance
+dirLight.shadow.camera.far = distance
+dirLight.shadow.bias = - 0.0001
+// spotLight = new THREE.SpotLight(0xffa95c,4)
+// spotLight.castShadow = true
 // spotLight.shadow.bias = -20
 // spotLight.shadow.mapSize.width = 1024*4
 // spotLight.shadow.mapSize.height = 1024*4
-scene.add(new THREE.SpotLightHelper(spotLight))
-camera.add( spotLight.target );
-spotLight.target.position.set( 0, 0, -2 )
+// scene.add(new THREE.SpotLightHelper(spotLight))
+// camera.add( spotLight.target );
+// spotLight.target.position.set( 0, 0, -2 )
 pointLight = new THREE.PointLight(0xffa95c,4,30)
 pointLight.position.set(0,0,0)
 // pointLight.castShadow = true
 // pointLight.shadow.bias = -0.001
 // pointLight.shadow.mapSize.width = 1024*4
 // pointLight.shadow.mapSize.height = 1024*4
-scene.add(pointLight)
+// scene.add(pointLight)
 // scene.add(new THREE.PointLightHelper(pointLight))
 // -------control of the light---------
 //
@@ -136,7 +162,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 //
 
 function animate() {
-spotLight.position.copy( camera.position )
+// spotLight.position.copy( camera.position )
 // spotLight.position.set(
 // 	camera.position.x + 10,
 // 	camera.position.y + 10,
